c9b6213bb936dafd7be10f908adc0a83c1daa3bc
Christiaan du Preez <christiaan.d@jobjack.co.za>
2023-11-08 17:30:28 +0200

    Start of something new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,104 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core'
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'util-project';
+export class AppComponent implements OnInit {
+  data: string = ''
+  title = 'util-project'
+  decryptInput = new FormControl('')
+  encryptInput = new FormControl('')
+  oneWayEncryptInput = new FormControl('')
+  oneWayEncryptOutput = new FormControl('')
+
+  ngOnInit(): void {
+    this.oneWayEncryptOutput.disable()
+  }
+
+  decrypt() {
+    const apiUrl = 'http://localhost:1000/decrypt'; // Replace with your API endpoint
+    const postData = {
+      data: this.decryptInput.getRawValue()
+    };
+
+    fetch(apiUrl, {
+      method: 'POST', // HTTP method (e.g., POST)
+      headers: {
+        'Content-Type': 'application/json', // Set the content type to JSON
+      },
+      body: JSON.stringify(postData), // Convert the data to JSON string
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Response data:', data);
+        this.encryptInput.setValue(data.data)
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  encrypt() {
+    const apiUrl = 'http://localhost:1000/encrypt'; // Replace with your API endpoint
+    const postData = {
+      data: this.encryptInput.getRawValue()
+    };
+
+    fetch(apiUrl, {
+      method: 'POST', // HTTP method (e.g., POST)
+      headers: {
+        'Content-Type': 'application/json', // Set the content type to JSON
+      },
+      body: JSON.stringify(postData), // Convert the data to JSON string
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Response data:', data);
+        this.decryptInput.setValue(data.data)
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  oneWayEncrypt() {
+    const apiUrl = 'http://localhost:1000/one-way-encrypt'; // Replace with your API endpoint
+    const postData = {
+      data: this.oneWayEncryptInput.getRawValue()
+    };
+
+    fetch(apiUrl, {
+      method: 'POST', // HTTP method (e.g., POST)
+      headers: {
+        'Content-Type': 'application/json', // Set the content type to JSON
+      },
+      body: JSON.stringify(postData), // Convert the data to JSON string
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Response data:', data);
+        this.oneWayEncryptOutput.setValue(data.data)
+        navigator.clipboard.writeText(this.oneWayEncryptOutput.getRawValue() ?? '');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
 }
